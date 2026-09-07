433c57cf67dcca84c0320dd741da1c124ab7772a
Arsh Malik <arsh.malik.ali@gmail.com>
2026-09-07 22:53:23 +0530

    Time: 0 ms (100%), Space: 46.6 MB (72.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,12 +14,12 @@ public class Solution {
         ListNode fast=head;
         ListNode slow=head;
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
-              if(fast==slow){
+            slow=slow.next;
+            if(fast==slow){
                 return true;
             }
         }
-    return false;
+        return false;
     }
 }
